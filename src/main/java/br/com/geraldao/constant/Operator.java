d8abfdf0f02192eea79e0d1e14170db2604a9b9c
Victor Hugo <victor.bello@voxage.com.br>
2018-03-08 16:53:45 -0300

    JavaDocs implementations

--- src/main/java/br/com/geraldao/constant/Operator.java
+++ src/main/java/br/com/geraldao/constant/Operator.java
@@ -1,14 +1,62 @@
 package br.com.geraldao.constant;
 
+/**
+ * SQL Operators to define which where clause use.
+ * <ul>
+ * <li>{@link #EQUAL}</li>
+ * <li>{@link #NOT_EQUAL}</li>
+ * <li>{@link #LESS_THAN_OR_EQUAL}</li>
+ * <li>{@link #STARTS_WITH}</li>
+ * <li>{@link #ENDS_WITH}</li>
+ * <li>{@link #CONTAINS}</li>
+ * <li>{@link #IN}</li>
+ * <li>{@link #NOT_IN}</li>
+ * <li>{@link #NOT_NULL}</li>
+ * <li>{@link #ISNULL}</li>
+ * </ul>
+ * 
+ * @author victor.bello
+ *
+ */
 public enum Operator {
+    /**
+     * Indicates that where clause should check for equality. EG: SELECT * FROM Table WHERE id = fieldValue
+     */
     EQUAL,
+    /**
+     * Indicates that where clause should check whether the field starts with the field value. EG: SELECT * FROM Table WHERE name LIKE 'fieldValue%'
+     */
     STARTS_WITH,
+    /**
+     * Indicates that where clause should check whether the field ends with the field value. EG: SELECT * FROM Table WHERE name LIKE '%fieldValue'
+     */
     ENDS_WITH,
+    /**
+     * Indicates that where clause should check whether the field contains the field value. EG: SELECT * FROM Table WHERE name LIKE '%fieldValue%'
+     */
     CONTAINS,
+    /**
+     * Indicates that where clause should check whether field is contained in a list of values. EG: SELECT * FROM Table WHERE id IN(1,2,3,4,5)
+     */
     IN,
+    /**
+     * Indicates that where clause should check whether field is not contained in a list of values. EG: SELECT * FROM Table WHERE id NOT IN(1,2,3,4,5)
+     */
     NOT_IN,
+    /**
+     * Indicates that where clause should check whether field is less or equal to the field value. EG: SELECT * FROM Table WHERE id <> fieldValue
+     */
     LESS_THAN_OR_EQUAL,
+    /**
+     * Indicates that where clause should check for inequality. EG: SELECT * FROM Table WHERE id IS NOT NULL
+     */
     NOT_EQUAL,
+    /**
+     * Indicates that where clause should check for not null field. EG: SELECT * FROM Table WHERE id IS NOT NULL
+     */
     NOT_NULL,
+    /**
+     * Indicates that where clause should check for null. EG: SELECT * FROM Table WHERE id IS NULL
+     */
     ISNULL;
 }
--- src/main/java/br/com/geraldao/predicate/PredicateBuilder.java
+++ src/main/java/br/com/geraldao/predicate/PredicateBuilder.java
@@ -1,9 +1,19 @@
 package br.com.geraldao.predicate;
 
+import javax.persistence.Entity;
 import javax.persistence.criteria.Predicate.BooleanOperator;
 
 import br.com.geraldao.constant.Operator;
+import br.com.geraldao.dao.BaseService;
 
+/**
+ * 
+ * Class responsible to build <code>PredicateClause</code> used on <code> BaseService </code> to create JPA predicates query
+ * 
+ * @see PredicateClause
+ * @see BaseService
+ * @author victor.bello
+ */
 public class PredicateBuilder {
 
     private PredicateClause predicate;
@@ -12,65 +22,199 @@ public class PredicateBuilder {
         this.predicate = predicate;
     }
 
+    /**
+     * Starts the where clause of a query using <code>{@link Operator#EQUAL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @param fieldValue
+     *            - Condition value.
+     * @return {@link PredicateBuilder}
+     */
     public static PredicateBuilder where(String fieldName, Object fieldValue) {
         return where(fieldName, Operator.EQUAL, fieldValue);
     }
 
+    /**
+     * Starts the where clause of a query using <code>{@link Operator#ISNULL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @return {@link PredicateBuilder}
+     */
+    public static PredicateBuilder whereIsNull(String fieldName) {
+        return where(fieldName, Operator.ISNULL, null);
+    }
+
+    /**
+     * Starts the where clause of a query using <code>{@link Operator#NOT_NULL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @return {@link PredicateBuilder}
+     */
+    public static PredicateBuilder whereIsNotNull(String fieldName) {
+        return where(fieldName, Operator.NOT_NULL, null);
+    }
+
+    /**
+     * Starts the where clause of a query using selected <code>{@link Operator}</code>
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @param operator
+     *            - Condition that should be applied of <code>{@link Operator}</code>
+     * @param fieldValue
+     *            - Condition value.
+     * @return @return {@link PredicateBuilder}
+     */
     public static PredicateBuilder where(String fieldName, Operator operator, Object fieldValue) {
         PredicateClause qp = new PredicateClause();
         qp.getQueries().add(new Condition(fieldName, operator, fieldValue));
         return new PredicateBuilder(qp);
     }
 
+    /**
+     * Add a <b>conjunction</b> on current created where clause using <code>{@link Operator#NOT_NULL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @return @return {@link PredicateBuilder}
+     */
     public PredicateBuilder andIsNotNull(String fieldName) {
         return and(fieldName, Operator.NOT_NULL, null);
     }
 
+    /**
+     * Add a <b>conjunction</b> on current created where clause using <code>{@link Operator#ISNULL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @return @return {@link PredicateBuilder}
+     */
     public PredicateBuilder andIsNull(String fieldName) {
         return and(fieldName, Operator.ISNULL, null);
     }
 
+    /**
+     * Add a <b>conjunction</b> on current created where clause using <code>{@link Operator#EQUAL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @param fieldValue
+     *            - Condition value.
+     * @return {@link PredicateBuilder}
+     */
     public PredicateBuilder and(String fieldName, Object fieldValue) {
         return and(fieldName, Operator.EQUAL, fieldValue);
     }
 
+    /**
+     * Add a <b>conjunction</b> on current created where clause using selected <code>{@link Operator}</code>
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @param operator
+     *            - Condition that should be applied of <code>{@link Operator}</code>
+     * @param fieldValue
+     *            - Condition value.
+     * @return @return {@link PredicateBuilder}
+     */
     public PredicateBuilder and(String fieldName, Operator operator, Object fieldValue) {
         add(BooleanOperator.AND, fieldName, operator, fieldValue);
         return this;
     }
 
+    /**
+     * Add a <b>disjunction</b> on current created where clause using <code>{@link Operator#NOT_NULL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @return @return {@link PredicateBuilder}
+     */
     public PredicateBuilder orIsNotNull(String fieldName) {
         return or(fieldName, Operator.NOT_NULL, null);
     }
 
+    /**
+     * Add a <b>disjunction</b> on current created where clause using <code>{@link Operator#ISNULL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @return @return {@link PredicateBuilder}
+     */
     public PredicateBuilder orIsNull(String fieldName) {
         return or(fieldName, Operator.ISNULL, null);
     }
 
+    /**
+     * Add a <b>disjunction</b> on current created where clause using <code>{@link Operator#EQUAL}</code> as default
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @param fieldValue
+     *            - Condition value.
+     * @return {@link PredicateBuilder}
+     */
     public PredicateBuilder or(String fieldName, Object fieldValue) {
         return or(fieldName, Operator.EQUAL, fieldValue);
     }
 
+    /**
+     * Add a <b>disjunction</b> on current created where clause using selected <code>{@link Operator}</code>
+     * 
+     * @param fieldName
+     *            - Property name on class mapped by <code>{@link Entity}</code> annotation
+     * @param operator
+     *            - Condition that should be applied of <code>{@link Operator}</code>
+     * @param fieldValue
+     *            - Condition value.
+     * @return @return {@link PredicateBuilder}
+     */
     public PredicateBuilder or(String fieldName, Operator operator, Object fieldValue) {
         add(BooleanOperator.OR, fieldName, operator, fieldValue);
         return this;
     }
 
-    public PredicateBuilder andBlock(PredicateClause q) {
+    /**
+     * Add predicates using a <b>conjunction</b> as connector with the previously created predicates <br>
+     * EG: SELECT * FROM Table WHERE field=1 <b>AND (field=3 and field=4 or field=5)</b>
+     * 
+     * @param predicateClause
+     *            - Previously created and {@link PredicateBuilder#build()} predicates
+     * @return {@link PredicateBuilder}
+     */
+    public PredicateBuilder andBlock(PredicateClause predicateClause) {
         checkPredicateState();
-        q.setOperatorCombiner(BooleanOperator.AND);
-        predicate.getQueries().add(q);
+        predicateClause.setOperatorCombiner(BooleanOperator.AND);
+        predicate.getQueries().add(predicateClause);
         return this;
     }
 
-    public PredicateBuilder orBlock(PredicateClause q) {
+    /**
+     * Add predicates using a <b>disjunction</b> as connector with the previously created predicates <br>
+     * EG: SELECT * FROM Table WHERE field=1 <b>OR (field=3 and field=4 or field=5)</b>
+     * 
+     * @param predicateClause
+     *            - Previously created and {@link PredicateBuilder#build()} predicates
+     * @return {@link PredicateBuilder}
+     */
+    public PredicateBuilder orBlock(PredicateClause predicateClause) {
         checkPredicateState();
-        q.setOperatorCombiner(BooleanOperator.OR);
-        predicate.getQueries().add(q);
+        predicateClause.setOperatorCombiner(BooleanOperator.OR);
+        predicate.getQueries().add(predicateClause);
         return this;
     }
 
+    /**
+     * Build {@link PredicateClause} according with auxiliary methods
+     * 
+     * @return {@link PredicateClause}
+     * @see PredicateBuilder
+     */
     public PredicateClause build() {
+        PredicateClause predicate = this.predicate;
+        this.predicate = null;
         return predicate;
     }
 
@@ -83,7 +227,7 @@ public class PredicateBuilder {
 
     private void checkPredicateState() {
         if (predicate == null) {
-            throw new IllegalStateException("Invalid state for variable predicate. Probably wrong usage of PredicateBuilder.");
+            throw new IllegalStateException("Invalid state for variable predicate. If you already called build() method you should restart the process of generating a PredicateClause");
         }
     }
 
